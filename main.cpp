4bcaea78eb8cbaf294f0e50e15b5a4be4976e511
dawidg81 <dawidgorski.m@gmail.com>
2026-02-26 20:15:43 +0100

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,63 @@
 #include <iostream>
 
 int main(){
-	bool in_game = false;
-	int board[3][3] = {
+	bool in_game = true;
+	int board[3][3] = { // 0 is nothing, 1 is circle, 2 is cross
 		{0, 0, 0},
 		{0, 0, 0},
 		{0, 0, 0}
 	};
-	int board_state = 0;
 	int player_state = 0; // 0 is circle, 1 is cross
 
 	int input = 0;
 	while(in_game){
 		if(player_state == 0) std::cout << "Circle's turn\n";
 		else std::cout << "Crosse's turn\n";
+
+		std::cout << "_____________\n"
+		       	  << "| " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " |\n"
+			  << "|---|---|---|\n"
+			  << "| " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " |\n"
+			  << "|---|---|---|\n"
+			  << "| " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " |\n"
+			  << "-------------\n";
+
+		std::cout << "Select cell number to mark: ";
 		std::cin >> input;
 		if(std::cin.fail()){
 			std::cout << "Input failed\n";
 		}
 		switch(input){
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
-			case 6: break;
-			case 7: break;
-			case 8: break;
-			case 9: break;
+			case 1:
+				if(player_state == 0 && board[0][0] == 0){
+					board[0][0] = 1;
+				} else if(player_state == 1 && board[0][0] == 0){
+					board[0][0] = 2;
+				}
+
+				if(player_state == 0){
+					player_state = 1;
+				} else if(player_state == 1){
+					player_state = 0;
+				}
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case 9:
+				break;
+			default: std::cout << "Invalid number\n";
 		}
 	}
 }
