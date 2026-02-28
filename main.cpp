a51d0dcad06485ccf20fde01fc12f6764282794f
dawidg81 <dawidgorski.m@gmail.com>
2026-02-28 11:49:07 +0100

    Added main menu and player nicknames

--- main.cpp
+++ main.cpp
@@ -36,68 +36,88 @@ std::string stateName(int playerState) {
 }
 
 int main() {
+  std::string player1, player2;
   bool inGame = true;
   int board[3][3] = {// 0 is nothing, 1 is circle, 2 is cross
                      {0, 0, 0},
                      {0, 0, 0},
                      {0, 0, 0}};
   int playerState = 0; // 0 is circle, 1 is cross
-
   int input = 0;
-  while (inGame) {
-    if (playerState == 0)
-      std::cout << "Circle's turn.\n";
-    else
-      std::cout << "Crosse's turn.\n";
+  int gameInput = 0;
 
-    std::cout << "_____________\n"
-              << "| " << symbol(board[0][0]) << " | " << symbol(board[0][1])
-              << " | " << symbol(board[0][2]) << " |\n"
-              << "|---|---|---|\n"
-              << "| " << symbol(board[1][0]) << " | " << symbol(board[1][1])
-              << " | " << symbol(board[1][2]) << " |\n"
-              << "|---|---|---|\n"
-              << "| " << symbol(board[2][0]) << " | " << symbol(board[2][1])
-              << " | " << symbol(board[2][2]) << " |\n"
-              << "-------------\n";
+  std::cout << "= MAIN MENU =\n"
+               "1. Local Game\n";
+  std::cout << "Select> ";
+  std::cin >> input;
 
-    if (checkWin(board, 1)) {
-      std::cout << "Circle has won!";
-      inGame = false;
-    }
+  if (std::cin.fail()) {
+    std::cout << "Input failed.\n";
+    return 1;
+  }
 
-    if (checkWin(board, 2)) {
-      std::cout << "Cross has won!";
-      inGame = false;
-    }
+  if (input == 1) {
 
-    if (inGame) {
-      std::cout << "Select> ";
-      std::cin >> input;
-    }
+    std::cout << "Enter nickname for Player 1: ";
+    std::cin >> player1;
+    std::cout << "Enter nickname for player 2: ";
+    std::cin >> player2;
 
-    if (input == 0) {
-      std::cout << stateName(playerState) << " has forfeited.\n";
-      inGame = false;
-    }
+    while (inGame) {
+      if (playerState == 0)
+        std::cout << player1 << "'s (Circle) turn.\n";
+      else
+        std::cout << player2 << "'s (Cross) turn.\n";
 
-    if (input < 0 || input > 9) {
-      std::cout << "No such cell.\n";
-      continue;
-    } else {
-      board[(input - 1) / 3][(input - 1) % 3] = playerState + 1;
-    }
+      std::cout << "_____________\n"
+                << "| " << symbol(board[0][0]) << " | " << symbol(board[0][1])
+                << " | " << symbol(board[0][2]) << " |\n"
+                << "|---|---|---|\n"
+                << "| " << symbol(board[1][0]) << " | " << symbol(board[1][1])
+                << " | " << symbol(board[1][2]) << " |\n"
+                << "|---|---|---|\n"
+                << "| " << symbol(board[2][0]) << " | " << symbol(board[2][1])
+                << " | " << symbol(board[2][2]) << " |\n"
+                << "-------------\n";
 
-    if (playerState == 0) {
-      playerState = 1;
-    } else if (playerState == 1) {
-      playerState = 0;
-    }
+      if (checkWin(board, 1)) {
+        std::cout << player1 << " (Circle) has won!";
+        inGame = false;
+      }
 
-    if (std::cin.fail()) {
-      std::cout << "Input failed.\n";
-      inGame = false;
-      return 1;
+      if (checkWin(board, 2)) {
+        std::cout << player2 << " (Cross) has won!";
+        inGame = false;
+      }
+
+      if (inGame) {
+        std::cout << "Select> ";
+        std::cin >> gameInput;
+      }
+
+      if (gameInput == 0) {
+        std::cout << stateName(playerState) << " has forfeited.\n";
+        inGame = false;
+      }
+
+      if (gameInput < 0 || gameInput > 9) {
+        std::cout << "No such cell.\n";
+        continue;
+      } else {
+        board[(gameInput - 1) / 3][(gameInput - 1) % 3] = playerState + 1;
+      }
+
+      if (playerState == 0) {
+        playerState = 1;
+      } else if (playerState == 1) {
+        playerState = 0;
+      }
+
+      if (std::cin.fail()) {
+        std::cout << "gameInput failed.\n";
+        inGame = false;
+        return 1;
+      }
     }
   }
 }
