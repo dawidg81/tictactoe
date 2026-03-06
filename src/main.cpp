754c63ec50071929afc8eb08ad1727286c7d7130
dawidg81 <dawidgorski.m@gmail.com>
2026-03-06 19:19:24 +0100

    Added logic.cpp; moved checkWin to logic.cpp from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
-
-bool checkWin(int board[3][3], int player) {
-	for (int i = 0; i < 3; i++) {
-		if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-			return true;
-		if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-			return true;
-	}
-
-	if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-		return true;
-	if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-		return true;
-
-	return false;
-}
+#include "utils.hpp"
 
 char symbol(int v) {
 	if (v == 0) {
